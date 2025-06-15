8e6b2e6421be5b16e3e849bea2f570aaea7a2017
midoriiro <alexis.bekhdadi@gmail.com>
2025-06-15 14:20:16 +0000

    feat(ui): add theme configuration

---
+++ ui/src/theme.js
@@ -0,0 +1,12 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        mode: 'dark',
+        primary: {
+            main: '#607d8b', // blue-grey
+        },
+    },
+});
+
+export default theme;
